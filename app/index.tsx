ce14c7ddfadc0769eb58dd2c84e8d20ce294dbd8
zentoo31 <zentoo31@gmail.com>
2025-09-29 14:01:03 -0500

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Pressable, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView className="flex-1 bg-gray-100 items-center justify-center">
+      <Text className="text-2xl font-bold text-blue-600">
+        🚀 Expo + NativeWind + SafeArea
+      </Text>
+      <Pressable className="mt-4 px-4 py-2 bg-blue-500 rounded-lg">
+        <Text className="text-white font-medium">¡Presióname!</Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+}
