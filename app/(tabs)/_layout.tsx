6a879677b1d1a8bacd84c1c06c7bcec777981703
Diego Pineda <zentoo31@gmail.com>
2025-09-30 13:36:11 -0500

    Add TabLayout component and update text in Index component

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+export class TabLayout {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  }
+}
+
+export default TabLayout;
--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-gray-100 items-center justify-center">
       <Text className="text-2xl font-bold text-blue-600">
-        🚀 Expo + NativeWind + SafeArea
+        🚀 Expo + xd + NativeWind + SafeArea
       </Text>
       <Pressable className="mt-4 px-4 py-2 bg-blue-500 rounded-lg">
         <Text className="text-white font-medium">¡Presióname!</Text>
