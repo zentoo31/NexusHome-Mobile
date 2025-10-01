6ee36e35c158f453e3ff842fbe5ef712b678d72b
Diego Pineda <zentoo31@gmail.com>
2025-10-01 12:19:28 -0500

    Refactor Lights component to use SafeAreaView and improve structure

--- app/(tabs)/lights.tsx
+++ app/(tabs)/lights.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-const lights = () => {
+const Lights = () => {
   return (
-    <View>
-      <Text>lights</Text>
-    </View>
+    <SafeAreaView className='bg-[#0f0f0f] flex flex-row h-full p-4'>
+      <Text className='color-white'>lights</Text>
+    </SafeAreaView>
   )
 }
 
-export default lights
+export default Lights
