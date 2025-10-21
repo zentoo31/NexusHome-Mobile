feec612a6419e20594110fbf07ec83a17e38c6dc
zentoo31 <zentoo31@gmail.com>
2025-10-20 22:54:42 -0500

    Fix background color in Config component

--- app/(tabs)/config.tsx
+++ app/(tabs)/config.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Config = () => {
   return (
-    <SafeAreaView className='bg-[#0f0f0f] flex flex-row h-full p-4'>
+    <SafeAreaView className='bg-[#111] flex flex-row h-full p-4'>
       <Text className='color-white'>Config</Text>
     </SafeAreaView>
   )
